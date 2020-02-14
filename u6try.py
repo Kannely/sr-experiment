1cfd3c234f804e81b831ec3eaf4027d2049a344f
Agathe <blu.kannely@gmail.com>
2020-02-14 08:42:35 +0100

    set config get feedback

--- u6try.py
+++ u6try.py
@@ -7,16 +7,40 @@ Desc: first implementation try
 from datetime import datetime
 import u6  # Import the u6 module
 
+resolutionIndex = 1
+gainIndex = 0
+settlingFactor = 0
+differential = False
+duration = 10   # en secondes
 
 # Open first found U6
 d = u6.U6()
 
-
-# ligne 2206 : class Timer(FeedbackCommand)
-# 1623 : def loadConfig(self, configParserObj): (u6)
+# Basic config (1623)
+d.loadConfig('config/FT9498_fichier_calibration.cal')
 
 #Allume la led ?
 d.getFeedback(u6.LED(True))
+
+# Read
+feedbackArguments = []
+#feedbackArguments.append(u6.DAC0_8(Value=125))
+feedbackArguments.append(u6.PortStateRead())
+
+for i in range(0, 10, 2):
+    feedbackArguments.append(u6.AIN(i))
+
+start = datetime.now()
+delta = 0
+while delta < duration:
+    results = d.getFeedback(feedbackArguments)
+    delta = datetime.now() - start
+
+end = datetime.now()
+delta = end - start
+
+print("Execution time :" + delta)
+print(results)
 
 # When all done
 d.close()
